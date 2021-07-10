9a690387d76306b53ef72475f0db24901d72eb6c
Vitaliy <v.a.grahov@ya.ru>
2021-07-10 23:43:08 +0500

    Initial commit

---
+++ app/src/main/java/ru/gravitana/gvd_universe/MainActivity.kt
@@ -0,0 +1,18 @@
+package ru.gravitana.gvd_universe
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import ru.gravitana.gvd_universe.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
